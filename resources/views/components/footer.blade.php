c038da8e00b64c0e8fd127cc69888538605a312b
Jayden <jaydenhorrevoets2007@gmail.com>
2024-09-23 11:04:48 +0200

    ticket 1

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,49 +1,31 @@
-
 <footer>
-	© {{ __('misc.copyright') }}
-    <!-- Blokje "Over ons" -->
-    <div id="about-us">
-        <h4>Over ons</h4>
-        <p>Wij zijn een bedrijf dat zich richt op het leveren van uitstekende producten en diensten. Meer informatie komt binnenkort!</p>
-    </div>
+    <div class="container">
+        © <span id="year"></span> {{ __('misc.copyright') }}
+        <!-- Blokje "Over ons" -->
+        <div id="about-us">
+            <h4>Over ons</h4>
+            <p>Wij zijn een bedrijf dat zich richt op het leveren van uitstekende producten en diensten. Meer informatie komt binnenkort!</p>
+        </div>
 
-    <!-- Contactgegevens -->
-    <div id="contact-info">
-        <h4>Contact</h4>
-        <p>Email: info@voorbeeld.nl</p>
-        <p>Telefoon: 012-3456789</p>
-        <p>Adres: Straatnaam 123, 1234 AB Plaats</p>
-    </div>
+        <!-- Contactgegevens -->
+        <div id="contact-info">
+            <h4>Contact</h4>
+            <p>Email: info@voorbeeld.nl</p>
+            <p>Telefoon: 012-3456789</p>
+            <p>Adres: Straatnaam 123, 1234 AB Plaats</p>
+        </div>
 
-    <!-- Social Media Links -->
-    <div id="social-links">
-        <h4>Volg ons</h4>
-        <a href="https://facebook.com">Facebook</a> |
-        <a href="https://twitter.com">Twitter</a> |
-        <a href="https://instagram.com">Instagram</a>
+        <!-- Social Media Links -->
+        <div id="social-links">
+            <h4>Volg ons</h4>
+            <a href="https://facebook.com">Facebook</a> |
+            <a href="https://twitter.com">Twitter</a> |
+            <a href="https://instagram.com">Instagram</a>
+        </div>
     </div>
 </footer>
 
-
-<!-- analytics code -->
+<!-- Voeg dit JavaScript toe voor dynamisch jaar -->
 <script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-30506707-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
-<!-- Einde analytics code -->
-
-<script language="Javascript" type="text/javascript">
-
- if (top.location!= self.location) {
-  top.location = self.location.href
- }
-
+    document.getElementById("year").textContent = new Date().getFullYear();
 </script>
