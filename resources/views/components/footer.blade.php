96dc099c19cee11e84f47977532cbf47085ff81c
Jayden <jaydenhorrevoets2007@gmail.com>
2024-09-23 10:57:13 +0200

    ticket 6
    footer uitgebreid

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,10 +1,31 @@
 
 <footer>
 	© {{ __('misc.copyright') }}
+    <!-- Blokje "Over ons" -->
+    <div id="about-us">
+        <h4>Over ons</h4>
+        <p>Wij zijn een bedrijf dat zich richt op het leveren van uitstekende producten en diensten. Meer informatie komt binnenkort!</p>
+    </div>
+
+    <!-- Contactgegevens -->
+    <div id="contact-info">
+        <h4>Contact</h4>
+        <p>Email: info@voorbeeld.nl</p>
+        <p>Telefoon: 012-3456789</p>
+        <p>Adres: Straatnaam 123, 1234 AB Plaats</p>
+    </div>
+
+    <!-- Social Media Links -->
+    <div id="social-links">
+        <h4>Volg ons</h4>
+        <a href="https://facebook.com">Facebook</a> |
+        <a href="https://twitter.com">Twitter</a> |
+        <a href="https://instagram.com">Instagram</a>
+    </div>
 </footer>
 
 
-<!-- analytics code -->              
+<!-- analytics code -->
 <script type="text/javascript">
 
   var _gaq = _gaq || [];
@@ -19,10 +40,10 @@
 </script>
 <!-- Einde analytics code -->
 
-<script language="Javascript" type="text/javascript"> 
- 
- if (top.location!= self.location) { 
+<script language="Javascript" type="text/javascript">
+
+ if (top.location!= self.location) {
   top.location = self.location.href
- } 
- 
+ }
+
 </script>
